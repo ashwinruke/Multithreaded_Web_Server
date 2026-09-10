25357b053101691ba2b0f7a460264a0902192b24
Ashwin Kumar Ruke <74692179+ashwinruke@users.noreply.github.com>
2026-09-10 21:58:16 +0530

    Remove testing print statement
    Removed testing print statement for driftwatch.

--- api/app.py
+++ api/app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-print("Testing for driftwatch")
 
 @app.route("/api/user", methods=["POST"])
 def user():
